ba5d4e2f827db11afb60b762e7832a878fb17887
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-12-29 10:49:42 +0200

    Update: functionality button

--- my-app/components/compound/navigation-layout/navigation-auth-module.tsx
+++ my-app/components/compound/navigation-layout/navigation-auth-module.tsx
@@ -9,6 +9,22 @@ export default function NavAuthModule() {
     const spanStyles = 'flex bg-transparent rounded-full w-auto h-full items-center justify-end';
 
     return (
+        <>
+        <motion.span 
+            // hover effects
+            whileHover={{ scale: 1.1 }}
+            // whileHover={{ scale: 1.2, rotate: 90 }}
+            whileTap={{
+              scale: 0.9,
+            }}
+            // styles
+            className={twMerge(spanStyles)}>
+            {/* span menu here */}
+            <Button variant='subAction'>
+                <RxTarget size={28} />
+                Report
+            </Button>
+        </motion.span>
         <motion.span 
             // hover effects
             whileHover={{ scale: 1.1 }}
@@ -24,5 +40,6 @@ export default function NavAuthModule() {
                 See orders
             </Button>
         </motion.span>
+        </>
     )
 }
