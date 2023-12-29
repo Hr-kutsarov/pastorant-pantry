ef69b0d8921c0daee4ffbeece7a8ba1815ca7c2e
Hr-kutsarov <hr.kutsarov89@gmail.com>
2023-12-29 10:49:59 +0200

    Update: motion span logo

--- my-app/components/compound/navigation-layout/navigation-logo.tsx
+++ my-app/components/compound/navigation-layout/navigation-logo.tsx
@@ -4,12 +4,24 @@ import { motion } from "framer-motion";
 import { GiBookmarklet } from "react-icons/gi";
 
 export default function NavLogoModule() {
-    const spanStyles = 'bg-transparent group relative border-1 border-slate-50 flex aspect-square w-full max-w-[210px] max-h-[90px] h-full justify-center items-center';
+    // component styles
+    const spanStyles = 'bg-transparent group relative border-1 border-slate-50 flex aspect-square w-full max-w-[190px] max-h-[60px] h-full justify-center items-center';
 
     return (
-        <motion.span className={twMerge(spanStyles)}>
+        <motion.span 
+        initial={{ scale: 0, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+          delay: 0.3,
+          delayChildren: 1.5
+        }}
+        className={twMerge(spanStyles)}>
+            
             {/* span menu here */}
-            <p className="text-2xl font-bold tracking-tight text-slate-50">Pastorant pantry</p>
+            <p className="text-lg font-bold tracking-wide text-slate-50">Pastorant pantry</p>
         </motion.span>
     )
 }
