eb749615724d4536ce2499541d030c23b465a5cb
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-01-06 10:39:57 +0200

    Chore: commenting

--- my-app/components/compound/navigation-layout.tsx
+++ my-app/components/compound/navigation-layout.tsx
@@ -1,7 +1,13 @@
+// import libs
 import { twMerge } from "tailwind-merge"
+
+// import components
 import NavLogoModule from "@/components/compound/navigation-layout/navigation-logo";
 import NavMenuModule from "@/components/compound/navigation-layout/navigation-menu";
 import NavAuthModule from "@/components/compound/navigation-layout/navigation-auth-module";
+
+    {/* server  component containing three client components \\
+    (animated logo, nav menu and trigger buttons)*/}
 
 export default function NavLayout() {
     const navStyles = 'bg-transparent flex items-center gap-4 mb-4';
