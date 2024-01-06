77c3165c618eb80b6dc23b5fae4d727441e41154
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-01-06 10:40:15 +0200

    Update: Zustand store

--- my-app/hooks/orders.ts
+++ my-app/hooks/orders.ts
@@ -1,51 +1,48 @@
 import { create } from 'zustand';
+import { ProductType } from '@/lib/types';
 
-interface ProductItem {
-    name: string,
-    quantity: number,
-    needed: boolean,
-    category?: string
-}
-
-const baseList: ProductItem[] = [
-    {
-        name: 'abc',
-        quantity: 3,
-        needed: false,
-        category: 'test'
-    },
-    {
-        name: 'abcd',
-        quantity: 3,
-        needed: false,
-        category: 'test'
-    },
-    {
-        name: 'abcef',
-        quantity: 3,
-        needed: false,
-        category: 'test'
-    },
-    {
-        name: 'abcefg',
-        quantity: 3,
-        needed: false,
-        category: 'test'
-    },
-
+const baseList: ProductType[] = [
+    // {
+    //     id: 'asd-asd-123-123',
+    //     name: 'abc',
+    //     quantity: 3,
+    //     needed: false,
+    //     category: 'test'
+    // },
+    // {
+    //     id: 'asd-asd-124',
+    //     name: 'abcd',
+    //     quantity: 3,
+    //     needed: false,
+    //     category: 'test'
+    // },
+    // {
+    // id: 'asd-asd-125',
+    //     name: 'abcef',
+    //     quantity: 3,
+    //     needed: false,
+    //     category: 'test'
+    // },
+    // {
+    //     id: 'asd-asd-126',
+    //     name: 'abcefg',
+    //     quantity: 3,
+    //     needed: false,
+    //     category: 'test'
+    // },
 ]
 
 interface OrderProps {
-    itemsList: ProductItem[];
-    setList: (newList: ProductItem[]) => void;
-    addItem: (newItem: ProductItem) => void;
+    itemsList: ProductType[];
+    setList: (newList: ProductType[]) => void;
+    addItem: (newItem: ProductType) => void;
     clearList: () => void;
 }
 
 const useItemStorage = create<OrderProps>((set) => ({
     itemsList: baseList,
-    setList: (newList: ProductItem[]) => set({ itemsList: newList}),
-    addItem: (newItem: ProductItem) => set({ itemsList: [...baseList, newItem]}),
+    setList: (newList: ProductType[]) => set({ itemsList: newList}),
+    addItem: (newItem: ProductType) => set({ itemsList: [...baseList, newItem]}),
     clearList: () => set({ itemsList: []}),
 }))
 
