2a528fe32a22a1fac02588d8c98bf1e8b9eec986
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-01-06 10:42:43 +0200

    Chore: fixed Tailwind styles and Motion components

--- my-app/components/compound/section-layout.tsx
+++ my-app/components/compound/section-layout.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import { twMerge } from "tailwind-merge"
+import { motion, AnimatePresence } from "framer-motion"
 
 export default function SectionLayout({ children }: {children: React.ReactNode}) {
     return (
-        <section className={twMerge('bg-slate-200 w-full h-full flex flex-col p-2 lg:p-4 rounded-2xl shadow-md')}>
-            {children}
-        </section>
+        <AnimatePresence>
+            <motion.section 
+            initial={{y: 20}}
+            animate={{y: 0}}
+            transition={{ease: 'easeIn', duration: 0.7, delay: 0.1}}
+            className={twMerge('bg-slate-200/10 w-full h-full mb-4 flex flex-col rounded-lg shadow-md')}>
+                {children}
+            </motion.section>
+        </AnimatePresence>
     )
 }
