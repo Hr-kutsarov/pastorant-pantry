6523ec91c3d1b098b4cf9cd7d9e20d55e9db2171
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-01-13 14:36:38 +0200

    Fix: (TEMPORARY!) projectId now recognized

--- my-app/db/database.ts
+++ my-app/db/database.ts
@@ -1,18 +1,20 @@
-// firebase init 
+// Import the functions you need from the SDKs you need
 
-import { initializeApp } from 'firebase/app';
-
-// options
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
+    projectId: "pastorant-pantry-3f73f",
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID
-}
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  };
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// export firebase singleton
-
-export const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
