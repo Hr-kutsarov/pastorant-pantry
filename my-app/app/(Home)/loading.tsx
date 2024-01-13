0cd1f6dbf47522e235982f270bb57783b854be28
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-01-13 14:30:20 +0200

    Update: improved loading page layouts

---
+++ my-app/app/(Home)/loading.tsx
@@ -0,0 +1,10 @@
+import PageLayout from '@/components/compound/page-layout'
+import LoadingDotsAnimation from '@/components/compound/loading/LoadingThreeDotsAnimation'
+
+export default function LoadingAuthPage() {
+    return (
+        <PageLayout>
+            <LoadingDotsAnimation />
+        </PageLayout>
+    )
+}
